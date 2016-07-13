6f8c088e96632a42acbcb053fafa5d2268672993
Kyle Zaragoza <popwarsweet@gmail.com>
2016-07-13 16:09:28 +0200

    limits contentOffset to scroll views content size

--- ScrollableGridCollectionView/Source/View/Supplementary View/ScrollViewSupplementaryView.swift
+++ ScrollableGridCollectionView/Source/View/Supplementary View/ScrollViewSupplementaryView.swift
@@ -55,8 +55,11 @@ class ScrollViewSupplementaryView: UICollectionReusableView {
         // note section should be set *before* contentSize/contentOffset so delegate is not called w/ incorrect section
         section = svAttributes.section
         scrollView.contentSize = svAttributes.contentSize
-        // using animated=false api to stop any leftover momentum after cell is reused
-        scrollView.setContentOffset(svAttributes.contentOffset, animated: false)
+        // using animated=false api to stop any leftover momentum after cell is reused,
+        // using min/max offset to ensure we don't set a contentOffset beyond contentSize
+        let maxAllowableXOffset = svAttributes.contentSize.width - scrollView.bounds.width
+        let xContentOffset = min(maxAllowableXOffset , max(0, svAttributes.contentOffset.x))
+        scrollView.setContentOffset(CGPoint(x: xContentOffset, y: svAttributes.contentOffset.y), animated: false)
     }
     
     
